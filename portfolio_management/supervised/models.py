69ebc618c8f7d5328eda6c4a20ef65d7cd8210c1
Thomas <hirtzt@gmail.com>
2021-06-20 19:03:45 +0200

    Initial commit supervised

---
+++ portfolio_management/supervised/models.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+class BasicModel(nn.Module):
+    def __init__(self, num_observations, num_properties: int = 10):
+        super(BasicModel, self).__init__()
+        self.num_properties = num_properties
+        self.num_observations = num_observations
+        self.linear = nn.Linear(num_properties*num_observations, num_properties)
+
+    def forward(self, observation: torch.Tensor):
+        observation = torch.flatten(observation, start_dim=1)
+        return self.linear(observation)
